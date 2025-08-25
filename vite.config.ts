7c48773d07dee7cb9eb59678da39a03489f9becf
GorkemTikic <grkmtkc94@gmail.com>
2025-08-25 07:37:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/balance-log/", // <-- change if your repo name is different
-});
+  base: '/balance-log/',   // 👈 THIS LINE is the important fix
+})
